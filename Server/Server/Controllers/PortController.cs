d902b53358f80a8d0f373df8f853f9a6f5665c8f
nikibor <nborgolov96@gmail.com>
2016-11-18 13:16:15 +0400

    ToDO: подключить БД к проекту

---
+++ Server/Server/Controllers/PortController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Server.Controllers
+{
+    public class PortController : Controller
+    {
+        // GET: Port
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        //public ActionResult CreateUser()
+        //{
+        //    return "asd";
+        //}
+    }
+}
