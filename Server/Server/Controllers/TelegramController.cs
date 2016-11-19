1fe7dc3e67351db1272b3bdd86a514aac735b3a1
nikibor <nborgolov96@gmail.com>
2016-11-19 11:50:11 +0400

    Сделал запросы и сохранение в БД

--- Server/Server/Controllers/TelegramController.cs
+++ Server/Server/Controllers/TelegramController.cs
@@ -22,9 +22,10 @@ namespace Server.Controllers
             {
                 string Connection = @"Server=tcp:nikitaborgolov.database.windows.net,1433;Initial Catalog=DataBase;Persist Security Info=False;User ID=nborgolov96;Password=Nikita207968811;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
                 SqlConnection connection = new SqlConnection(Connection);
-                SqlCommand command = new SqlCommand("INSERT INTO [dbo].[Users] ([Id], [Name], [Adress], [TelegramId]) VALUES (2, 'Nikita', 'Borgolov', 2)");
-                
                 connection.Open();
+                SqlCommand command = new SqlCommand("INSERT INTO [dbo].[Users] ([Id], [Name], [Adress], [TelegramId]) VALUES (20, 'Nikita', 'Borgolov', 2)", connection);
+                command.ExecuteNonQuery();
+                connection.Close();
                 return "ВСё ХоРОшО";
             }
             catch(Exception ex)
